d788c2d5bfcff6ede90d8ffb8201776c9524c4d1
Alex Guo <xfguo@credosemi.com.cn>
2016-02-27 11:34:59 +0800

    [wip] Fix KARCH issue, use riscv for riscv64.
    Signed-off-by: Alex Guo <xfguo@credosemi.com.cn>

--- include/kernel.mk
+++ include/kernel.mk
@@ -68,7 +68,7 @@ else ifneq (,$(findstring $(ARCH) , arceb ))
   LINUX_KARCH := arc
 else ifneq (,$(findstring $(ARCH) , armeb ))
   LINUX_KARCH := arm
-else ifneq (,$(findstring $(ARCH), riscv))
+else ifneq (,$(findstring $(ARCH), riscv64))
   LINUX_KARCH := riscv
 else ifneq (,$(findstring $(ARCH) , mipsel mips64 mips64el ))
   LINUX_KARCH := mips
