9235088cf33b5c73a1073ff2fe21e6cf6b247f62
Alex Guo <xfguo@credosemi.com.cn>
2018-02-21 17:15:54 +0800

    now it can boot and log into openwrt.
    - some tmp script.
    - inittab
    - use armvirt as reference
    
    Signed-off-by: Alex Guo <xfguo@credosemi.com.cn>

---
+++ qemu.sh
@@ -0,0 +1,9 @@
+/data/riscv/freedom-u-sdk/work/riscv-qemu/prefix/bin/qemu-system-riscv64 \
+	-nographic \
+	-machine virt \
+	-kernel bbl/bbl \
+	-drive file=bin/targets/riscv64/generic-glibc/openwrt-riscv64-ext4.img,format=raw,id=hd0 \
+	-device virtio-blk-device,drive=hd0 \
+	-netdev user,id=net0 \
+	-device virtio-net-device,netdev=net0 \
+
